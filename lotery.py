b51883e852f6f2cb07f447489a76e9e095c58b83
Sonia Delgado <soniaemi2003@hotmail.com>
2024-02-26 19:23:36 +0100

    Lotery

---
+++ lotery.py
@@ -0,0 +1,16 @@
+
+def weighted_lottery (weights):
+    list_container=[]
+    for clave, valor in weights.items():
+        for _ in range(valor):
+            list_container.append(clave)
+    return list_container
+
+other_weights = {
+        'winning': 1,
+        'break_even': 2,
+        'losing': 3
+    }
+
+print(weighted_lottery(other_weights))
+
---
+++ media.py
@@ -0,0 +1,21 @@
+import math
+
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+index_list= len(sale_prices)
+list_sorted= sorted(sale_prices)
+
+print (list_sorted)
+firslist= list_sorted[:math.floor(index_list/2)]
+print (firslist)
+
